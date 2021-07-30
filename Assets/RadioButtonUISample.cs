0e250cb304fed52b5fdb744645a7af25225a7094
kksumm123 <kksumm123@gmail.com>
2021-07-30 15:59:20 +0900

    토글들 가져오는 로직 추가

--- Assets/RadioButtonUISample.cs
+++ Assets/RadioButtonUISample.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RadioButtonUISample : MonoBehaviour
 {
-    
+    Toggle toggle1;
+    Toggle toggle2;
+    Toggle toggle3;
+
+    void Awake()
+    {
+        toggle1 = transform.Find("ToggleGroup/Toggle1").GetComponent<Toggle>();
+        toggle2 = transform.Find("ToggleGroup/Toggle2").GetComponent<Toggle>();
+        toggle3 = transform.Find("ToggleGroup/Toggle3").GetComponent<Toggle>();
+    }
 }
