bb4750a669bab59b9e312d2710d9bad13f9d9a60
kksumm123 <kksumm123@gmail.com>
2021-07-30 16:44:42 +0900

    마지막 선택된 인덱스 저장해서 가져오도록

--- Assets/DropDownUISameple.cs
+++ Assets/DropDownUISameple.cs
@@ -5,15 +5,18 @@ using UnityEngine.UI;
 
 public class DropDownUISameple : MonoBehaviour
 {
-
     Dropdown dropdown;
+    SaveInt lastSelectedIndex;
     void Start()
     {
         dropdown = transform.Find("Dropdown").GetComponent<Dropdown>();
         dropdown.onValueChanged.AddListener(OnValueChanged);
+        lastSelectedIndex = new SaveInt("lastSelectedIndex");
+        dropdown.value = lastSelectedIndex.Value;
     }
     void OnValueChanged(int selectedIndex)
     {
+        lastSelectedIndex.Value = selectedIndex;
         string selectedText = dropdown.options[selectedIndex].text;
         print(selectedText);
         transform.Find("Selected/Text").GetComponent<Text>().text = selectedText;
