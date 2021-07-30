db004b9c1b29750835e0860bafb8c318fc645ff2
kksumm123 <kksumm123@gmail.com>
2021-07-30 16:07:59 +0900

    함수 중복 리팩토링

--- Assets/RadioButtonUISample.cs
+++ Assets/RadioButtonUISample.cs
@@ -15,20 +15,16 @@ public class RadioButtonUISample : MonoBehaviour
         toggle2 = transform.Find("ToggleGroup/Toggle2").GetComponent<Toggle>();
         toggle3 = transform.Find("ToggleGroup/Toggle3").GetComponent<Toggle>();
 
-        toggle1.onValueChanged.AddListener(OnValueChanged1);
-        toggle2.onValueChanged.AddListener(OnValueChanged2);
-        toggle3.onValueChanged.AddListener(OnValueChanged3);
+        toggle1.onValueChanged.AddListener((bool bChecked) => { OnValueChanged(toggle1, bChecked); });
+        toggle2.onValueChanged.AddListener((bool bChecked) => { OnValueChanged(toggle2, bChecked); });
+        toggle3.onValueChanged.AddListener((bool bChecked) => { OnValueChanged(toggle3, bChecked); });
     }
-    void OnValueChanged1(bool bChecked)
+    void OnValueChanged(Toggle toggle, bool bChecked)
     {
-        print($"OnValueChanged1 : {bChecked}");
-    }
-    void OnValueChanged2(bool bChecked)
-    {
-        print($"OnValueChanged2 : {bChecked}");
-    }
-    void OnValueChanged3(bool bChecked)
-    {
-        print($"OnValueChanged3 : {bChecked}");
+        if (bChecked)
+        {
+            string text = toggle.GetComponentInChildren<Text>().text;
+            print($"{text} 변경됨 : {bChecked}");
+        }
     }
 }
