fe376015b33e767e70f2ce3a8379f4851ca416e9
kksumm123 <kksumm123@gmail.com>
2021-07-30 16:56:41 +0900

    1. QueryUI 작성중 2. Canvas에 TestQueryUI추가 및 작성 시작

--- Assets/QueryUISample.cs
+++ Assets/QueryUISample.cs
@@ -1,8 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QueryUISample : MonoBehaviour
 {
-
+    Button button;
+    void Start()
+    {
+        button = transform.Find("ButtonGroup/Button").GetComponent<Button>();
+    }
 }
