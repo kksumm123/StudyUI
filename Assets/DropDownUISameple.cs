6a8c74eff7e380bf4898a8c27e2aa616ecea3a26
kksumm123 <kksumm123@gmail.com>
2021-07-30 16:39:22 +0900

    DDL.cs 작성완료, 동작 잘됨

--- Assets/DropDownUISameple.cs
+++ Assets/DropDownUISameple.cs
@@ -1,8 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DropDownUISameple : MonoBehaviour
 {
-    
+    Dropdown dropdown;
+    void Start()
+    {
+        dropdown = transform.Find("Dropdown").GetComponent<Dropdown>();
+        dropdown.onValueChanged.AddListener(OnValueChanged);
+    }
+    void OnValueChanged(int selectedIndex)
+    {
+        print(dropdown.options[selectedIndex].text);
+    }
 }
